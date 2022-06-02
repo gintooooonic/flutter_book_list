2bd3331d66874e295cedde4bb3ae9ccd97f30b57
Woojin Shin <gintooooonic@gmail.com>
2022-06-02 23:23:07 +0900

    feat: DetailScreen 레이아웃 작성

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -5,9 +5,55 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('책 제목'),
+        title: Text('쉽고 빠른 플러터 앱 개발'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Image.network(
+              'https://simage.kyobobook.co.kr/images/book/large/514/l9791165921514.jpg'),
+          Padding(
+            padding: EdgeInsets.all(3),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        '쉽고 빠른 플러터 앱 개발',
+                        style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Flutter & Dart로 화면 구현ㆍ상태 관리ㆍ데이터 처리ㆍ디자인 패턴 익히기',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Center(
+                    child: Icon(
+                  Icons.star,
+                  color: Colors.red,
+                )),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
