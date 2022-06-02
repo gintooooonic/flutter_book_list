3500437b7df4b3c7b8dcd77deae930c7dada1c5c
Woojin Shin <gintooooonic@gmail.com>
2022-06-02 23:52:42 +0900

    feat: 아이콘과 책 설명 추가

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -47,13 +47,70 @@ class DetailScreen extends StatelessWidget {
                 width: MediaQuery.of(context).size.width * 0.15,
                 padding: EdgeInsets.all(10),
                 child: Center(
-                    child: Icon(
-                  Icons.star,
-                  color: Colors.red,
-                )),
-              )
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
             ],
-          )
+          ),
+          Padding(
+            padding: EdgeInsets.all(3),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Icon(
+                    Icons.call,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Contact',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.near_me,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Route',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.save,
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    'Save',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              '인프런의 인기 강의, “무작정 시리즈” 제작자가 직접 집필한 플러터 도서. 초심자들도 어렵지 않은, 쉽고 빠른 플러터 앱 개발!',
+            ),
+          ),
         ],
       ),
     );
